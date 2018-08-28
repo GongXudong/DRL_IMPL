826f77599f809a76b0d3a9e5d7027397c993cedd
gongxudong <gongxudong_cs@aliyun.com>
2018-08-29 00:19:46 +0800

    add content in lenient.temp

--- Lenient/temperature_record.py
+++ Lenient/temperature_record.py
@@ -4,14 +4,24 @@ import math
 
 class Temp_record(object):
 
-    def __init__(self, shape: tuple, beta_len, beta_p=-0.01, beta_d=0.95):
+    def __init__(self, shape: tuple, beta_len, beta_p=-0.01, beta_d=0.95, v=1., u=0.999):
+        """
+
+        :param shape: (s, a)
+        :param beta_len: 
+        :param beta_p:
+        :param beta_d:
+        :param v:
+        :param u:
+        """
         self.temps = np.ones(shape, dtype=np.float32)
         self.betas = np.zeros(beta_len)
         self.beta_len = beta_len
         self.beta_p = beta_p
         self.beta_d = beta_d
         self._init_beta()
-
+        self.v = v
+        self.u = u
 
     def _init_beta(self):
         tmp = self.beta_p * 1.
@@ -25,7 +35,20 @@ class Temp_record(object):
     def convert_to_tuple(self, s: np.ndarray, a: int) -> tuple:
         return tuple(s) + (a, )
 
+    def get_state_action_temp(self, s: np.ndarray, a: int):
+        s_a_tuple = self.convert_to_tuple(s, a)
+        return self.temps[s_a_tuple]
 
+    def get_state_temp(self, s: np.ndarray):
+        cur_s = tuple(s)
+        return np.mean(self.temps[cur_s])
+
+    def decay_temp(self, episode):
+        # decay the episode
+
+
+        # decay v
+        self.v *= self.u
 
 if __name__ == '__main__':
     obj = Temp_record((1, 2), 1000)
